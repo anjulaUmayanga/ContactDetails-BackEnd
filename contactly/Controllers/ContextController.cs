15ec85be0f34df0feaa922cbb16de9c42f76ff25
anjulaUmayanga <anjulaumayanga047@gmail.com>
2024-12-28 01:28:57 +0530

    Add project files.

---
+++ contactly/Controllers/ContextController.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using contactly.Data;
+using contactly.Models;
+using contactly.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace contactly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContextController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+
+        public ContextController(ContactlyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllcontacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+        [HttpPost]
+        public IActionResult AddContact(AddContactsRequestDTO request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite
+            };
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+            return Ok(domainModelContact);
+
+           
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id)
+        {
+            var contact = dbContext.Contacts.Find(id);
+
+            if(contact is not null)
+            {
+                dbContext.Contacts.Remove(contact);
+                dbContext.SaveChanges();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ contactly/Data/ContactlyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using contactly.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace contactly.Data
+{
+    public class ContactlyDbContext : DbContext
+    {
+        public ContactlyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        
+    }
+}
